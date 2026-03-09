f06ad0d2a8ca0fcc89878f89001c575f6ad30dd3
robot <robot@local>
2026-03-09 15:05:09 +0900

    1

---
+++ hskbd45/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define VENDOR_ID       0x22CE
+#define PRODUCT_ID      0x0276
+
+/* Device metadata (DEVICE_VER / MANUFACTURER / PRODUCT / DESCRIPTION)
+   are provided by the keyboard info and must not be redefined here
+   to avoid conflicts with QMK's generated headers. */
